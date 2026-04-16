c64f9d6d7fe08f8b6652f67c2db55f70ea123f99
Septendecillion0 <syfan@andrew.cmu.edu>
2026-04-16 18:38:52 -0400

    Add level manager for scene changes, add expanded map

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager Instance;
+    private bool isLoading = false;
+
+    // current scene number
+    [SerializeField] private int currentLevelIndex;
+
+
+    // Temporary variables for keeping "score" across multiple days.
+    // TODO: rework or remove score manager to keep scoring in one place
+    public int encounter_count = 0;
+    public int total_violations = 0;
+    public int correct_reports = 0;
+    public int incorrect_reports = 0;
+    public int vampires_recorded = 0;
+    public int villagers_lost = 0;
+
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject); // Prevent duplicate managers
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Loads the next day and adds the score from the previous day
+    /// 
+    /// TODO: add protection to reload same scene if it is the last scene in the index
+    /// </summary>
+    public void NextDay()
+    {
+        if (isLoading) return;
+
+        //TOD
+        StartCoroutine(LoadScene(currentLevelIndex + 1));
+    }
+
+    /// <summary>
+    /// Reloads the current day and discards temporary score changes
+    /// </summary>
+
+    public void RestartDay()
+    {
+        if (isLoading) return;
+        StartCoroutine(LoadScene(currentLevelIndex));
+    }
+
+    /// <summary>
+    /// Loads the scene given by index
+    /// </summary>
+    private IEnumerator LoadScene(int index)
+    {
+        isLoading = true;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(index);
+        op.allowSceneActivation = false;
+
+        while (op.progress < 0.9f)
+        {
+            // await Task.Yield();
+            // ^placeholder cleanup task routine
+            yield return null; // wait a frame
+        }
+
+        op.allowSceneActivation = true;
+
+        isLoading = false;
+    }
+
+}
